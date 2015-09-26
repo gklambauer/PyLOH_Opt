e2cdedf577370070a3f110f713b68a2391a2943f
patrick-praher <patrick.praher@gmail.com>
2015-09-26 16:32:43 +0200

    Call all segments
    Include the read count from "NONE" LOH segments in the likelihood,
    calculation and make a copy number call on all segments (if no LOH info
    is available, only the read counts are used)

--- pyloh/model/poisson_model.py
+++ pyloh/model/poisson_model.py
@@ -95,8 +95,13 @@ class PoissonLatentVariables(LatentVariables):
         for j in range(0, J):
             LOH_status_j = self.data.segments[j][7]
             
+	    #print(str(j))
             if LOH_status_j == 'NONE':
+                xi_j, kappa_j, psi_j = self._sufficient_statistics_by_segment_NL(parameters, eta, j)
+                sufficient_statistics['kappa'][j, :] = kappa_j
+                sufficient_statistics['psi'][j, :] = psi_j
                 sufficient_statistics['xi'].append('NONE')
+                #print("worked!")
                 continue
                         
             xi_j, kappa_j, psi_j = self._sufficient_statistics_by_segment(parameters, eta, j)
@@ -106,6 +111,27 @@ class PoissonLatentVariables(LatentVariables):
             
         self.sufficient_statistics = sufficient_statistics
     
+    def _sufficient_statistics_by_segment_NL(self, parameters, eta, j):
+        a_T_j = 0
+        b_T_j = 0
+        d_T_j = a_T_j + b_T_j
+        I_j = 0
+        D_N_j = self.data.segments[j][4]
+        D_T_j = self.data.segments[j][5]
+        rho_j = parameters['rho'][j]
+        phi = parameters['phi']
+
+        c_S = self.restart_parameters['copy_number_base']
+        Lambda_S = self.data.segments.Lambda_S
+
+        xi_j = 0
+
+        kappa_j = 0
+
+        psi_j = self._get_psi_by_segment(D_N_j, D_T_j, phi, rho_j, kappa_j, eta)
+
+        return xi_j, kappa_j, psi_j
+ 
     def _sufficient_statistics_by_segment(self, parameters, eta, j):        
         a_T_j = self.data.paired_counts[j][:, 2]
         b_T_j = self.data.paired_counts[j][:, 3]
@@ -248,7 +274,8 @@ class PoissonModelParameters(ModelParameters):
             LOH_status_j = self.data.segments[j][7]
             
             if LOH_status_j == 'NONE':
-                continue            
+                complete_ll += self._complete_ll_CNA_by_segment(phi, psi[j], j)
+		continue            
 
             complete_ll += self._complete_ll_CNA_by_segment(phi, psi[j], j)
             complete_ll += self._complete_ll_LOH_by_segment(phi, xi[j], psi[j], j)
@@ -351,7 +378,7 @@ class PoissonModelParameters(ModelParameters):
             LOH_status_j = self.data.segments[j][7]
             
             if LOH_status_j == 'NONE':
-                copy_number_j = 'NONE'
+                copy_number_j = self._get_copy_number_by_segment(j)
             else:
                 copy_number_j = self._get_copy_number_by_segment(j)
                 
@@ -385,6 +412,8 @@ class PoissonModelLikelihood(ModelLikelihood):
             LOH_status_j = self.data.segments[j][7]
             
             if LOH_status_j == 'NONE':
+	        log_likelihood_j = self._get_log_likelihood_by_segment_LN(parameters, priors, j)
+        	log_likelihood += log_likelihood_j
                 continue
             
             log_likelihood_j = self._get_log_likelihood_by_segment(parameters, priors, j)
@@ -392,6 +421,55 @@ class PoissonModelLikelihood(ModelLikelihood):
         
         return log_likelihood
     
+    def _get_log_likelihood_by_segment_LN(self, parameters, priors, j):
+        C = self.config_parameters['copynumber_tumor_num']
+        G = self.config_parameters['genotypes_tumor_num']
+        c_N = np.array(constants.COPY_NUMBER_NORMAL)
+        c_T = np.array(self.config_parameters['copynumber_tumor'])
+        c_gT = np.array(self.config_parameters['copynumber_tumor_compat'])
+        c_S = self.restart_parameters['copy_number_base']
+        a_T_j = 0
+        b_T_j = 0
+        d_T_j = a_T_j + b_T_j
+        D_N_j = self.data.segments[j][4]
+        D_T_j = self.data.segments[j][5]
+        Lambda_S = self.data.segments.Lambda_S
+        rho_j = parameters['rho'][j]
+        phi = parameters['phi']
+        omega = priors['omega']
+        p_CG = self.config_parameters['P_CG']
+        eps = constants.EPS
+
+        c_E_j = get_c_E(c_N, c_T, phi)
+        c_E_s = get_c_E(c_N, c_S, phi)
+        lambda_E_j = D_N_j*c_E_j*Lambda_S/c_E_s
+
+        mu_N = np.array(constants.MU_N)
+        mu_T = np.array(self.config_parameters['MU_T'])
+        mu_E_c = get_mu_E(mu_N, mu_T, c_N, c_gT, phi)
+
+        #log-likelihood for LOH
+        #ll_LOH_j = np.zeros(C)
+
+        #for c in range(0, C):
+        #    log_likelihoods = np.log(p_CG[c]) + log_binomial_likelihood(b_T_j, d_T_j, mu_E_c)
+        #    log_likelihoods = np.logaddexp.reduce(log_likelihoods, axis = 1)
+        #    ll_LOH_j[c] = log_likelihoods.sum()
+
+        #log-likelihood for CNA
+        ll_CNA_j = log_poisson_likelihood(D_T_j, lambda_E_j)
+
+        ll_j = np.log(rho_j) + ll_CNA_j
+
+        ll_j = np.logaddexp.reduce(ll_j, axis = 1)[0]
+
+        #Dirichlet priors for each segment
+        ll_j = ll_j + log_dirichlet_pdf(rho_j, omega)
+
+        return ll_j
+
+
+
     def _get_log_likelihood_by_segment(self, parameters, priors, j):
         C = self.config_parameters['copynumber_tumor_num']
         G = self.config_parameters['genotypes_tumor_num']
